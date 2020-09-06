b3859eae35844d3fe9c470659848d8d06c542f7e
Martin <martin.beck@utexas.edu>
2020-09-05 21:51:21 -0700

    Modified User information

--- mern-backend/models/user.js
+++ mern-backend/models/user.js
@@ -4,7 +4,6 @@ const userSchema = mongoose.Schema({
     id: mongoose.Schema.Types.ObjectId,
     username: {type: String, require: true},
     password: {type: String, require: true},
-    company: {type: String, require: true}
 });
 
 module.exports = mongoose.model('User', userSchema)
