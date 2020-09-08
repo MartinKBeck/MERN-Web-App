0cea02c85f71e0854757398b41c5d2b2cae629d6
Martin <martin.beck@utexas.edu>
2020-09-08 00:52:45 -0700

    Added user home information, removed 304 constant requests that come from frontend componentdidupdate function

--- mern-frontend/src/components/inventory-list.js
+++ mern-frontend/src/components/inventory-list.js
@@ -2,15 +2,61 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 
+const Inventory = props => (
+    <tr>
+        <td>{props.inventory.description}</td>
+        <td>{props.inventory.quantity}</td>
+    </tr>
+)
+
 export default class InventoryList extends Component {
     constructor(props) {
         super(props);
+        this.state = {inventory: []}
     }
 
-    render(){
+    componentDidMount() {
+        axios.get('http://localhost:4000/inv/')
+        .then(response => {
+            this.setState({inventory: response.data});
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
+
+    // componentDidUpdate(){
+    //     axios.get('http://localhost:4000/inv/')
+    //     .then(response => {
+    //         this.setState({inventory: response.data});
+    //     })
+    //     .catch(function (error) {
+    //         console.log(error);
+    //     })
+    // }
+
+    inventoryList(){
+        return this.state.inventory.map(function(currentInventory, i) {
+            return <Inventory inventory={currentInventory} key={i} />;
+        })
+    }
+
+    render() {
         return (
             <div>
                 <h3>Inventory List</h3>
+                <table className="table table-striped" style={{marginTop:20}}>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Quantity</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.inventoryList()}
+                    </tbody>
+                </table>
             </div>
         )
     }
