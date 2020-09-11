1d0e0d2a66b35611c0d102b5adb64abc26117f6f
Martin <martin.beck@utexas.edu>
2020-09-10 21:01:29 -0700

    Failure on heroku, changed app.use

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(__dirname + 'client/build'))
+    app.use(express.static('client/build'))
 
     app.get('/*', function(req, res) {
         res.sendFile(path.resolve(__dirname, 'client/build', 'client/build/index.html'))
