704893e5f4f30decc70a22d0d988c600e9d87188
Martin <martin.beck@utexas.edu>
2020-09-10 21:19:33 -0700

    Changed path.resolve

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + 'client/build/'))
 
     app.get('/*', function(req, res) {
-        res.sendFile(path.resolve(__dirname, 'client/build/', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client/build/'))
     })
 }
 
