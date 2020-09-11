9a7babf70d8c89c200fbd36ddaf1bdd5985aa44d
Martin <martin.beck@utexas.edu>
2020-09-10 21:15:49 -0700

    no directory client/build/public/index.html/ changed directory to work

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ if (process.env.NODE_ENV !== 'production') {
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(__dirname + 'client/build/public'))
+    app.use(express.static(__dirname + 'client/build/'))
 
     app.get('/*', function(req, res) {
-        res.sendFile(path.resolve(__dirname, 'client/build/public', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client/build/', 'index.html'))
     })
 }
 
